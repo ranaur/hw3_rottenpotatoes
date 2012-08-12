33d5148245e6b123c63550b199ea9cb7cc655bd8
saasbook <saasbook@saasbook.(none)>
2012-08-12 15:48:16 -0700

    HW3 part 1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,16 @@
 # Add a declarative step here for populating the DB with movies.
-
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+      movie_record = Movie.new( {          \
+        :title => movie["title"],                 \
+        :rating => movie["rating"],               \
+        :release_date => movie["release_date"]    \
+      } )
+    movie_record.save
   end
-  flunk "Unimplemented"
+#flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #  flunk "Unimplemented"
+  flunk " #(e1) should appear before #(e2)" unless page.content.index(e1) < page.content.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
